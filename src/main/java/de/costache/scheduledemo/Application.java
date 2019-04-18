4f0f66b8153478862ac76bc845c5f430b4a3597a
j.prakash.india@gmail.com <j.prakash.india@gmail.com>
2019-04-18 23:38:53 +0800

    spring boot with quartz

--- src/main/java/de/costache/scheduledemo/Application.java
+++ src/main/java/de/costache/scheduledemo/Application.java
@@ -1,8 +1,10 @@
 package de.costache.scheduledemo;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -11,5 +13,17 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Bean
+	org.h2.tools.Server h2Server() {
+	    Server server = new Server();
+	    try {
+	        server.runTool("-tcp");
+	        server.runTool("-tcpAllowOthers");
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	    return server;
+	}	
 
 }
